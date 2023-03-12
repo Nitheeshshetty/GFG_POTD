07711b233fc4f6d7236db93ca2eba0105e0be4ca
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-12 14:52:31 +0530

    Create Binary matrix having maximum number of 1s.java

---
+++ Binary matrix having maximum number of 1s.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class GfG {
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int B = sc.nextInt();
+            int arr[][] = new int[B][B];
+            for(int i = 0; i < B; i++) {
+                for(int j = 0; j < B; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            Solution obj = new Solution();
+            int ans[] = obj.findMaxRow(arr, B);
+            for(int i = 0; i < 2; i++) {
+                out.print(ans[i] + " ");
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int[] findMaxRow(int mat[][], int N) {
+        int r=0;
+        int cnt=0;
+        int j=N-1;
+        for(int i=0;i<N;i++)
+        {
+            while(j>=0 && mat[i][j]==1)
+            {
+                r=i;
+                j--;
+            }
+            cnt=N-j-1;
+        }
+        int ans[]=new int[2];
+        ans[0]=r;
+        ans[1]=cnt;
+        return ans;
+    }
+};
