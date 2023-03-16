7211c5243f37a944130b36af18bb82cba5e150b9
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-03-16 22:12:17 +0530

    Create Maximum occured integer.java

---
+++ Maximum occured integer.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class Main {
+
+    	public static void main (String[] args) throws IOException{
+    	    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    
+		    //taking size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int L[] = new int[n];
+		    int R[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements to array L
+		    for(int i=0; i<n; i++){
+		        L[i]=Integer.parseInt(inputLine[i]);
+		    }
+		    inputLine = br.readLine().trim().split(" ");
+		    int maxx = Integer.MIN_VALUE;
+		    
+		    //adding elements to array R
+		    for(int i=0; i<n; i++){
+		        R[i]=Integer.parseInt(inputLine[i]);
+		        if(R[i] > maxx){
+		            maxx=R[i];
+		        }
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxOccured() function
+		    System.out.println(obj.maxOccured(L, R, n, maxx));
+		}
+	}
+	
+}
+
+// L[] and R[] are input ranges
+// n : size of array
+// maxx: maximum element in R[]
+// arr[]: declared globally with size equal to 1000000
+
+class Solution{
+    //Function to find the maximum occurred integer in all ranges.
+    public static int maxOccured(int L[], int R[], int n, int maxx){
+        int prefix[] = new int[1000001];
+        for(int i=0;i<n;i++)
+        {
+            prefix[L[i]]++;
+            prefix[R[i]+1]--;
+        }
+        
+        int msum = prefix[0];
+		int res = 0;
+        for(int i=1;i<maxx;i++)
+        {
+            prefix[i] += prefix[i-1];
+            if (msum < prefix[i]) 
+            {
+				msum = prefix[i];
+				res = i;
+            }
+        }
+        return res;
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
