0bfc803c5f4d0cb75a7f8446a5ccbc7310df66c4
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-04 16:23:25 +0530

    Create Minimum Steps Required.java

---
+++ Minimum Steps Required.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			String str = br.readLine();
+			Solution obj = new Solution();
+			System.out.println(obj.minSteps(str));
+		}
+	}
+}
+
+
+
+
+class Solution
+{
+	int minSteps(String str) 
+  {
+		 int cnt=0;
+		 for(int i=1;i<str.length();i++)
+		 {
+		    if(str.charAt(i-1)!=str.charAt(i)){
+		        cnt++;
+		    }
+		 }
+		 if(cnt%2!=0){
+		    return cnt/2+2;
+		 }
+		 return cnt/2+1;
+	}
+}
