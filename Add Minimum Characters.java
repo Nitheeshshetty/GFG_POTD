f8b317bf7dec59d3438a9acf9d23d458abe7af11
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-08 21:15:15 +0530

    Update Add Minimum Characters.java

--- Add Minimum Characters.java
+++ Add Minimum Characters.java
@@ -41,7 +41,7 @@ class GFG
         int t = Integer.parseInt(read.readLine());
         while(t-- > 0)
         {
-        	String str = read.readLine();
+            String str = read.readLine();
             Solution ob = new Solution();
             System.out.println(ob.addMinChar(str));
         } 
