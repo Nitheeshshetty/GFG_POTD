17b18acfa9eae8948c687342d7a760253b999076
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-11 09:49:53 +0530

    Update Maximum Intersecting Lines.java

--- Maximum Intersecting Lines.java
+++ Maximum Intersecting Lines.java
@@ -1,10 +1,11 @@
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
-    public static void main(String[] args) throws IOException {
+class GFG 
+{
+    public static void main(String[] args) throws IOException 
+    {
         BufferedReader br =
             new BufferedReader(new InputStreamReader(System.in));
         int t =
