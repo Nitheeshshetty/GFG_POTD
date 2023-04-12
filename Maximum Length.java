eb04e39ce32be5687288694280a59df27d5654aa
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-12 20:59:28 +0530

    Update Maximum Length.java

--- Maximum Length.java
+++ Maximum Length.java
@@ -1,20 +1,19 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
-
-class FastReader{ 
+class FastReader
+{ 
     BufferedReader br; 
     StringTokenizer st; 
-
-    public FastReader(){ 
+    public FastReader()
+    { 
         br = new BufferedReader(new InputStreamReader(System.in)); 
     } 
 
-    String next(){ 
-        while (st == null || !st.hasMoreElements()){ 
+    String next()
+    { 
+        while (st == null || !st.hasMoreElements())
+        { 
             try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
         } 
         return st.nextToken(); 
