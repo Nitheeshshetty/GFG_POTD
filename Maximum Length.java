090607d4d215234e756f84130450a62776fd88dc
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-12 21:00:09 +0530

    Update Maximum Length.java

--- Maximum Length.java
+++ Maximum Length.java
@@ -14,7 +14,14 @@ class FastReader
     { 
         while (st == null || !st.hasMoreElements())
         { 
-            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            try
+            { 
+                st = new StringTokenizer(br.readLine());
+            } 
+            catch (IOException  e)
+            {
+                e.printStackTrace(); 
+            } 
         } 
         return st.nextToken(); 
     } 
