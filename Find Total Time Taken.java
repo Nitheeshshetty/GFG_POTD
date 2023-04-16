19c88e76607c8fa038af7beb9b9b618676b2c00f
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-16 17:27:42 +0530

    Update Find Total Time Taken.java

--- Find Total Time Taken.java
+++ Find Total Time Taken.java
@@ -1,25 +1,24 @@
 import java.util.*;
 import java.io.*;
 
-class GFG {
+class GFG 
+{
     public static void main(String args[])throws IOException
     {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out=new PrintWriter(System.out);
         int t=Integer.parseInt(in.readLine());
-        while(t-->0){
-            
+        while(t-->0)
+        {    
             int n=Integer.parseInt(in.readLine().trim());
             int a[]=new int[n];
             String s[]=in.readLine().trim().split(" ");
-            for(int i=0;i<n;i++){
+            for(int i=0;i<n;i++)
                 a[i]=Integer.parseInt(s[i]);
-            }
             s=in.readLine().trim().split(" ");
             int time[]=new int[n];
-            for(int i=0;i<n;i++){
+            for(int i=0;i<n;i++)
                 time[i]=Integer.parseInt(s[i]);
-            }
             Solution ob=new Solution();
             long ans=ob.totalTime(n,a,time);
             out.println(ans);
