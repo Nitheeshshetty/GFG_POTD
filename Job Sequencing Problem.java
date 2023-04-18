bf5a29590f91c5e3a0611fc557d794eed3b1b020
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-18 18:11:06 +0530

    Update Job Sequencing Problem.java

--- Job Sequencing Problem.java
+++ Job Sequencing Problem.java
@@ -1,11 +1,12 @@
-//{ Driver Code Starts
 import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-class Job {
+class Job 
+{
     int id, profit, deadline;
-    Job(int x, int y, int z){
+    Job(int x, int y, int z)
+    {
         this.id = x;
         this.deadline = y;
         this.profit = z; 
@@ -13,27 +14,20 @@ class Job {
 }
 
 class GfG {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        //testcases
-		int t = Integer.parseInt(br.readLine().trim());
-		while(t-->0){
+	int t = Integer.parseInt(br.readLine().trim());
+	while(t-->0)
+	{
             String inputLine[] = br.readLine().trim().split(" ");
-            
-            //size of array
             int n = Integer.parseInt(inputLine[0]);
             Job[] arr = new Job[n];
             inputLine = br.readLine().trim().split(" ");
-            
-            //adding id, deadline, profit
             for(int i=0, k=0; i<n; i++){
                 arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
             }
-            
             Solution ob = new Solution();
-            
-            //function call
             int[] res = ob.JobScheduling(arr, n);
             System.out.println (res[0] + " " + res[1]);
         }
