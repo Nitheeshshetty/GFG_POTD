0b8bc3065f8b21a3edfbc3d9923a4f5acdd789dd
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-24 18:19:53 +0530

    Update Minimum Number.java

--- Minimum Number.java
+++ Minimum Number.java
@@ -30,12 +30,12 @@ class IntArray
 
 class GFG 
 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException 
+    {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t;
         t = Integer.parseInt(br.readLine());
         while(t-- > 0){
-            
             int n;
             n = Integer.parseInt(br.readLine());
             
