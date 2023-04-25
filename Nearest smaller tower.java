da4a49f7cc346d7f91ee9c09d55b83a09acd901a
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-25 16:17:40 +0530

    Update Nearest smaller tower.java

--- Nearest smaller tower.java
+++ Nearest smaller tower.java
@@ -2,8 +2,10 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-class GFG{
-	public static void main(String [] args) throws IOException {
+class GFG
+{
+    public static void main(String [] args) throws IOException 
+    {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int test = Integer.parseInt(br.readLine());
 		while(test-- > 0) {
