e1c0af4f8a438007ebbd2a4487a453f24c3805ab
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-28 16:06:50 +0530

    Update Easy Task.java

--- Easy Task.java
+++ Easy Task.java
@@ -4,11 +4,9 @@ import java.util.*;
 class GFG
 {   
     public static void main (String[] args) throws Exception
-    {
-        
+    {        
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out=new PrintWriter(System.out);
-                
+        PrintWriter out=new PrintWriter(System.out);           
         int t=Integer.parseInt(in.readLine());
         while(t-- > 0){
             int n=Integer.parseInt(in.readLine().trim());
