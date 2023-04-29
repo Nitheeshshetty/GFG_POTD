4ff0482beac0b41062a18801580069ecd2cbf3c9
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-29 18:47:55 +0530

    Create Find Number.java

---
+++ Find Number.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            
+            long n=Long.parseLong(in.readLine());
+            Solution ob=new Solution();
+            out.println(ob.findNumber(n));
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public long findNumber(long N)
+    {
+        String s="";
+        int []arr={1,3,5,7,9};
+        while(N>0)
+        {
+            long cur=N%5;
+            if(cur==0)
+            {
+                s=arr[4]+s;
+            }
+            else{
+                s=arr[(int)cur-1]+s;
+            }
+            if(N%5==0 && N>0)
+            {
+                N=N/5-1;
+            }
+            else
+            {
+                N=N/5;
+            }
+        }
+        return Long.parseLong(s);
+    }
+}
