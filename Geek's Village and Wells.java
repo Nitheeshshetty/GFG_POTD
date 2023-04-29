2d97d4e41d4f8c163a19a2c1cfece5731743ae60
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-29 18:52:43 +0530

    Update Geek's Village and Wells.java

--- Geek's Village and Wells.java
+++ Geek's Village and Wells.java
@@ -1,14 +1,15 @@
 import java.util.*;
 import java.io.*;
 
-class Gfg {
+class Gfg 
+{
     public static void main(String args[])throws IOException
     {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out=new PrintWriter(System.out);
         int t=Integer.parseInt(in.readLine());
-        while(t-->0){
-            
+        while(t-->0)
+        {  
             String s[]=in.readLine().trim().split(" ");
             int n=Integer.parseInt(s[0]);
             int m=Integer.parseInt(s[1]);
