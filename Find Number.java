05501994e15e0c4b58fbb30d1feb622646e74911
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-04-30 17:37:02 +0530

    Update Find Number.java

--- Find Number.java
+++ Find Number.java
@@ -8,8 +8,8 @@ class GFG
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out=new PrintWriter(System.out);
         int t=Integer.parseInt(in.readLine());
-        while(t-->0){
-            
+        while(t-->0)
+        {    
             long n=Long.parseLong(in.readLine());
             Solution ob=new Solution();
             out.println(ob.findNumber(n));
