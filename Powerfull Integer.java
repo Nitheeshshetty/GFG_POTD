b3b14608544dae603eafbedf17eabb3e0c2f7441
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-01 16:52:19 +0530

    Update Powerfull Integer.java

--- Powerfull Integer.java
+++ Powerfull Integer.java
@@ -8,7 +8,8 @@ class GFG
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out=new PrintWriter(System.out);
         int t=Integer.parseInt(in.readLine());
-        while(t-->0){
+        while(t-->0)
+        {
             int n=Integer.parseInt(in.readLine().trim());
             int a[][]=new int[n][2];
             for(int i=0;i<n;i++){
