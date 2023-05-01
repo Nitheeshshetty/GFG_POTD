1c528c1373757484cd9e9aac935320eef27495c2
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-01 16:53:35 +0530

    Update Powerfull Integer.java

--- Powerfull Integer.java
+++ Powerfull Integer.java
@@ -34,7 +34,8 @@ class Solution{
         for (int i=0; i < interval.length; i++)
             max = Math.max(max, interval[i][1]);
         int[] prefix = new int[max + 2];
-        for (int i=0; i < interval.length; i++) {
+        for (int i=0; i < interval.length; i++) 
+        {
             prefix[interval[i][0]]++;
             prefix[interval[i][1] + 1]--;
         }
