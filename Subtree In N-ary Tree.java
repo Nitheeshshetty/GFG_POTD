d0d65f4aa44d938fdd8b943f48ddac18223f10c7
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-02 16:10:43 +0530

    Update Subtree In N-ary Tree.java

--- Subtree In N-ary Tree.java
+++ Subtree In N-ary Tree.java
@@ -22,9 +22,9 @@ public class Main
                     curr = tree.add(Integer.parseInt(s[0]), null);
                 else if(s[i].equals(" "))
                     continue;
-                else if(!q.isEmpty() && s[i].equals("N")){
+                else if(!q.isEmpty() && s[i].equals("N"))
                     curr = q.remove();
-                } else if(!s[i].equals("N")){
+                else if(!s[i].equals("N")){
                     q.add(tree.add(Integer.parseInt(s[i]), curr));
                 }
             }
