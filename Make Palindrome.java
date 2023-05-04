26fd3fc481a2b83269ce9fca0b500a7e68433d1f
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-04 21:09:32 +0530

    Update Make Palindrome.java

--- Make Palindrome.java
+++ Make Palindrome.java
@@ -72,8 +72,10 @@ class Solution
                 return false;
         }
         int i=0,j=str.length()-1;
-        while(i<j){
-            if(str.charAt(i++)!=str.charAt(j--)) return false;
+        while(i<j)
+        {
+            if(str.charAt(i++)!=str.charAt(j--)) 
+                return false;
         }
         return true;
     }
