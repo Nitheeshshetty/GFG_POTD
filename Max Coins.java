de427401b817364c3febbfea488575e6ec453646
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-05 20:04:34 +0530

    Update Max Coins.java

--- Max Coins.java
+++ Max Coins.java
@@ -15,7 +15,8 @@ class GFG implements Runnable
             BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
             PrintWriter out=new PrintWriter(System.out);
             int t=Integer.parseInt(in.readLine());
-            while(t-->0){
+            while(t-->0)
+            {
                 int n=Integer.parseInt(in.readLine().trim());
                 int a[][]=new int[n][3];
                 for(int i=0;i<n;i++){
