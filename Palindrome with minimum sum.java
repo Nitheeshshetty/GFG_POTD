4cfbdeee51c3e4687bb3bcb58214012d8fa83210
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-12 16:25:01 +0530

    Update Palindrome with minimum sum.java

--- Palindrome with minimum sum.java
+++ Palindrome with minimum sum.java
@@ -48,10 +48,12 @@ class Solution
         char prev = s[0];
         for(int i = 0; i<=s.length/2;i++)
         {
-            if(s[i]!='?') {
+            if(s[i]!='?') 
+            {
                 prev = curr;
                 curr = s[i];
-                if(prev!= '?')diff += Math.abs(prev- curr);
+                if(prev!= '?')
+                    diff += Math.abs(prev- curr);
             }
             
         }
