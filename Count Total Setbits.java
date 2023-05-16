4460ebe1b9000e9a90be1eb8b77fed1c928cc20f
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-16 21:44:18 +0530

    Update Count Total Setbits.java

--- Count Total Setbits.java
+++ Count Total Setbits.java
@@ -20,8 +20,10 @@ class GFG
 }
 
 
-class Solution {
-    public static long countBits(long N) {
+class Solution 
+{
+    public static long countBits(long N) 
+    {
         long count = 0;
         for (int i = 0; i < 31; i++) {
             int x = 1 << i; 
