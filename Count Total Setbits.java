29f72975f580c81013ef6f72ea8ad3c41beebedc
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-16 21:44:37 +0530

    Update Count Total Setbits.java

--- Count Total Setbits.java
+++ Count Total Setbits.java
@@ -25,7 +25,8 @@ class Solution
     public static long countBits(long N) 
     {
         long count = 0;
-        for (int i = 0; i < 31; i++) {
+        for (int i = 0; i < 31; i++) 
+        {
             int x = 1 << i; 
             long y = (N + 1) / (x * 2) * x; 
             long z = (N + 1) % (x * 2) - x; 
