b6c8fe66f9f099cbdddca094b55c68490442b178
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-17 21:26:45 +0530

    Update Cutting Binary String.java

--- Cutting Binary String.java
+++ Cutting Binary String.java
@@ -7,7 +7,6 @@ class GFG
     { 
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        
         while(t-- > 0){
             String s = read.readLine();
             Solution ob = new Solution();
