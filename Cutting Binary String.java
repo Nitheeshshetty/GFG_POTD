2efe15cf249c5b6a6cbf5934408834067c9e63f3
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-17 21:28:52 +0530

    Update Cutting Binary String.java

--- Cutting Binary String.java
+++ Cutting Binary String.java
@@ -22,7 +22,7 @@ class Solution
     static int cuts(String str)
     {
         for (int i = 22; i >= 0; i--)
-			str = str.replaceAll(Long.toBinaryString((long) Math.pow(5, i)), "#");
-		return str.contains("0") ? -1 : str.length();
+	    str = str.replaceAll(Long.toBinaryString((long) Math.pow(5, i)), "#");
+	return str.contains("0") ? -1 : str.length();
     }
 }
