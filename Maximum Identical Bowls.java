6a4d2dc37e9dc18ab0888694b963fdcb2c51692b
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-29 17:42:39 +0530

    Update Maximum Identical Bowls.java

--- Maximum Identical Bowls.java
+++ Maximum Identical Bowls.java
@@ -47,14 +47,13 @@ class GFG
 }
 
 
-class Solution {
+class Solution 
+{
     public static int getMaximum(int N, int[] arr) 
     {
         long sum = 0;
         for(int i=0 ; i<arr.length ; i++)
-        {
             sum += arr[i];
-        }
         while(1>0)
         {
             if(sum % N == 0)
