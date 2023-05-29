e9e7d49b597a93e5784322e398eb2646a3ae6bfd
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-05-29 12:44:22 +0530

    Create CamelCase Pattern Matching.java

---
+++ CamelCase Pattern Matching.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            String[] Dictionary=read.readLine().split(" ");
+            String Pattern=read.readLine();
+            Solution ob = new Solution();
+            ArrayList <String> ans=ob.CamelCase(N,Dictionary,Pattern);
+            for(String u:ans)
+             System.out.print(u+" ");
+            System.out.println(); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    ArrayList<String> CamelCase(int N,String[] Dictionary,String Pattern)
+    {
+        ArrayList<String> list = new ArrayList<>();
+        for (String str : Dictionary) 
+        {
+            String cmp = "";
+            for (char ch : str.toCharArray()) 
+            {
+                if (ch >= 'A' && ch <= 'Z') 
+                    cmp += ch;
+                if (cmp.length() == Pattern.length()) 
+                    break;
+            }
+            if (cmp.equals(Pattern)) 
+                list.add(str);
+        }
+        Collections.sort(list);
+        if (list.isEmpty()) 
+            list.add("-1");
+        return list;
+    }
+}
