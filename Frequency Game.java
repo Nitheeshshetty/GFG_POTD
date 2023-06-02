221331089fc6dfcd9d03db6e7a650b49f52b1dcc
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-06-02 17:42:40 +0530

    Update Frequency Game.java

--- Frequency Game.java
+++ Frequency Game.java
@@ -7,19 +7,18 @@ class GFG
     {
         BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
+        while (t-- > 0) 
+        {
             int n = Integer.parseInt(in.readLine());
             int arr[] = new int[n];
-
             String s[]=in.readLine().trim().split(" ");
             for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
-
             Solution obj = new Solution();
             System.out.println(obj.LargButMinFreq(arr, n));
         }
     }
 }
-// } Driver Code Ends
+
 
 
 // User function Template for Java
