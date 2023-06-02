dbc7ba300b3bed9d44fb039b49aefc53956a1a1d
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-06-02 17:43:34 +0530

    Update Frequency Game.java

--- Frequency Game.java
+++ Frequency Game.java
@@ -27,10 +27,10 @@ class Solution
         HashMap<Integer,Integer> map=new HashMap<>();
         int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-
         for(Integer num : map.keySet()){
             if(map.get(num)<=min){
                 min=map.get(num);
