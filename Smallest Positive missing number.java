e9f56f13623386e6dd1b725fe639e41b48bf3354
Nitheesh Shetty <83216828+Nitheeshshetty@users.noreply.github.com>
2023-07-13 21:58:27 +0530

    Update Smallest Positive missing number.java

--- Smallest Positive missing number.java
+++ Smallest Positive missing number.java
@@ -25,21 +25,16 @@ class Main
 	PrintWriter out=new PrintWriter(System.out);
 	int t=Integer.parseInt(in.readLine().trim());
 	while(t-->0){
-			int n=Integer.parseInt(in.readLine().trim());
-			int[] arr=new int[n];
-			String s[]=in.readLine().trim().split(" ");
-			
-			//adding elements to the array
-			for(int i=0;i<n;i++)
-				arr[i]=Integer.parseInt(s[i]);
-				
-			Solution obj = new Solution();
-			
-			//calling missingNumber()
-			int missing = obj.missingNumber(arr,n);
-			out.println(missing);
-		}
-		out.close();
+	    int n=Integer.parseInt(in.readLine().trim());
+	    int[] arr=new int[n];
+	    String s[]=in.readLine().trim().split(" ");
+	    for(int i=0;i<n;i++)
+		arr[i]=Integer.parseInt(s[i]);
+	    Solution obj = new Solution();
+	    int missing = obj.missingNumber(arr,n);
+	    out.println(missing);
+	}
+	out.close();
     }
 }
 
